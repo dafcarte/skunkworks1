d5ff1d99ceb0a8ebcbc3754ba4336070f5b4da9a
Mr. X <dave.carter@gmail.com>
2021-03-31 12:09:11 -0700

    Update VB-Desktop-Setup.sh

--- VB-Desktop-Setup.sh
+++ VB-Desktop-Setup.sh
@@ -1,5 +1,12 @@
  #!/bin/bash
 
+#basics, update/upgrade
+sudo apt-get update
+sudo apt-get upgrade
+
+#this allows us to run the desktop in fullscreen from virtualbox
 sudo apt-get install virtualbox-dkms
 sudo apt-get install virtualbox-guest-dkms
 sudo apt-get install virtualbox-guest-x11
+
+
