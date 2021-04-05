84360fcfb573f8038efbd69ff3df5f57cfa4e210
Mr. X <dave.carter@gmail.com>
2021-04-05 08:16:02 -0400

    Create keyringSetup.sh

---
+++ keyringSetup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#using the https://wiki.gnome.org/Projects/Libsecret project
+
+sudo apt-get install libsecret-1-0 libsecret-1-dev
+cd /usr/share/doc/git/contrib/credential/libsecret
+sudo make
+git config --global credential.helper /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret
