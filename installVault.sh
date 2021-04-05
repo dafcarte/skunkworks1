17c11bc5c13ae76eb36f7c8b7fdb2049919cc5b7
Mr. X <dave.carter@gmail.com>
2021-04-05 08:49:11 -0400

    Update installVault.sh

--- installVault.sh
+++ installVault.sh
@@ -5,3 +5,6 @@
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get update && sudo apt-get install vault
+
+#add in commands to check the status of vault
+#would be nice to have a runbook on secure configuration, operation, and interactions with Vault
