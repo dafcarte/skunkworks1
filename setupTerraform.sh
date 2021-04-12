f73052350130de87bf15a65896dc7d2303ca6499
Mr. X <dave.carter@gmail.com>
2021-04-12 08:35:06 -0400

    Update setupTerraform.sh

--- setupTerraform.sh
+++ setupTerraform.sh
@@ -14,3 +14,19 @@ sudo apt-get update && sudo apt-get install terraform
 terraform -install-autocomplete
 
 #if you want to check to see if it worked, just type "terraform -help"
+
+# Workspaces!
+#
+# To set up workspaces we need to design variable files. More below.
+#
+# To set lots of variables, it is more convenient to specify their values in a 
+# variable definitions file (with a filename ending in either .tfvars or .tfvars.json)
+# and then specify that file on the command line with -var-file:
+
+# terraform apply -var-file="testing.tfvars"
+# details here: https://www.terraform.io/docs/language/values/variables.html
+
+
+
+
+
